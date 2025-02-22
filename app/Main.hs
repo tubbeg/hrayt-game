f519d152a842a7e7c237d7b28393de0618a2cef4
Tobias Gärdebro <tubbeg@gmail.com>
2025-02-22 19:31:19 +0100

    Can now draw rectangles on the screen. Created datatypes. Planning to use a map because it should hopefully be quite easy to lookup and update cells (rectangles).

--- app/Main.hs
+++ app/Main.hs
@@ -11,40 +11,60 @@ import Raylib.Core (
   setTargetFPS,
   clearBackground)
 import Raylib.Core.Text (drawText)
-import Raylib.Util.Colors (orange, white)
+import Raylib.Util.Colors (orange, white, magenta, green)
 import Raylib.Internal(WindowResources)
+import Raylib.Core.Shapes (drawRectangle)
+import Raylib.Types (Color)
+import Data.Map (Map, empty)
+
+newtype Position = Pos (Int,Int)
+type Game = Map Position Color
+
+tetrisBlockSize :: (Int,Int)
+tetrisBlockSize = (30, 30)
 
 
+drawTetrisBlock :: (Int, Int) -> Color -> IO ()
+drawTetrisBlock (x,y) color = do
+  drawRectangle x y width height color
+  return ()
+  where
+    (width, height) = tetrisBlockSize
 
-drawStuff :: IO ()
-drawStuff = do
+drawStuff :: Game -> IO Game
+drawStuff game = do
   clearBackground white
-  drawText "woah woooh" 100 500 100 orange
-  return ()
+  -- drawText "woah woooh" 100 500 100 orange
+  drawTetrisBlock (100,100) green 
+  drawTetrisBlock (300,200) magenta
+  return game
 
-drawFunc :: IO ()
-drawFunc = do
+drawFunc :: Game -> IO Game
+drawFunc game = do
   beginDrawing
-  drawStuff
+  gs <- drawStuff game
   endDrawing
-  return ()
+  return gs
 
 
 loadWindow :: IO WindowResources
 loadWindow = do
   setTargetFPS 60
-  initWindow 600 800 "my-window"
+  initWindow 500 800 "my-window"
 
-mainDraw :: Bool  -> IO ()
-mainDraw True = return ()
-mainDraw False = do
+mainDraw :: Bool -> Game -> IO ()
+mainDraw True _ = return ()
+mainDraw False game = do
   keepDrawing <- windowShouldClose 
-  drawFunc
-  mainDraw keepDrawing
+  updatedGameState <- drawFunc game
+  mainDraw keepDrawing updatedGameState
+
+initGame :: Game
+initGame = empty
 
 main :: IO ()
 main = do
   w <- loadWindow
-  mainDraw False
+  mainDraw False initGame
   closeWindow $ Just w
   return ()
