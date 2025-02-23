c9fe65c3f9f2ae04c2a6e4ce7c62dbcebc6e60bd
Tobias Gärdebro <tubbeg@gmail.com>
2025-02-23 09:16:35 +0100

    The draw function can now draw based on the content of the Game map. Will need a function to convert tetris positions to window positions.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE OverloadedLists #-}
 module Main where
 
 
@@ -15,29 +16,63 @@ import Raylib.Util.Colors (orange, white, magenta, green)
 import Raylib.Internal(WindowResources)
 import Raylib.Core.Shapes (drawRectangle)
 import Raylib.Types (Color)
-import Data.Map (Map, empty)
+import qualified Data.Map as M
 
-newtype Position = Pos (Int,Int)
-type Game = Map Position Color
+newtype Position = Pos (Int,Int) deriving (Eq, Ord)
+data CellState = Falling | Static
+data TetrisCell = Empty | Cell Color CellState
+type Game = M.Map Position TetrisCell
 
-tetrisBlockSize :: (Int,Int)
-tetrisBlockSize = (30, 30)
 
+tetrisSize :: (Int, Int)
+tetrisSize = (10,20)
+
+tetrisBlockSize :: Position
+tetrisBlockSize = Pos (30, 30)
 
 drawTetrisBlock :: (Int, Int) -> Color -> IO ()
 drawTetrisBlock (x,y) color = do
   drawRectangle x y width height color
   return ()
   where
-    (width, height) = tetrisBlockSize
+    f (Pos (a,b)) = (a,b)
+    (width, height) = f tetrisBlockSize
+
+drawCell :: (Int, Int) -> Maybe TetrisCell -> IO ()
+drawCell pos (Just (Cell color _)) = do
+  drawTetrisBlock pos color
+  return ()
+drawCell _ _ = do return ()
+
+drawTetrisBlockRow :: Int -> Int -> Game -> IO ()
+drawTetrisBlockRow x y game
+  | x <= 0 = return ()
+  | otherwise = do
+    drawCell (x,y) maybeCell
+    drawTetrisBlockRow (x - 1) y game
+    where
+      maybeCell = M.lookup (Pos (x,y))  game
+
+drawTetrisBlocks :: Int -> Game -> IO ()
+drawTetrisBlocks y game
+  | y <= 0 = return ()
+  | otherwise = do
+    drawTetrisBlockRow gx y game
+    drawTetrisBlocks (y - 1) game
+    return ()
+    where
+      (gx,_) = tetrisSize
 
 drawStuff :: Game -> IO Game
 drawStuff game = do
   clearBackground white
   -- drawText "woah woooh" 100 500 100 orange
-  drawTetrisBlock (100,100) green 
+  drawTetrisBlock (100,100) green
   drawTetrisBlock (300,200) magenta
+  drawTetrisBlocks gy game
   return game
+  where
+    (_,gy) = tetrisSize
 
 drawFunc :: Game -> IO Game
 drawFunc game = do
@@ -55,12 +90,13 @@ loadWindow = do
 mainDraw :: Bool -> Game -> IO ()
 mainDraw True _ = return ()
 mainDraw False game = do
-  keepDrawing <- windowShouldClose 
+  stopDrawing <- windowShouldClose
   updatedGameState <- drawFunc game
-  mainDraw keepDrawing updatedGameState
+  mainDraw stopDrawing updatedGameState
 
 initGame :: Game
-initGame = empty
+-- initGame = M.empty
+initGame = [(Pos (2,2), Cell magenta Falling)]
 
 main :: IO ()
 main = do
